e45faa5225e21db5432ad00a07a8c90348f0fa83
John Asmuth <jasmuth@gmail.com>
2026-05-21 22:21:14 -0400

    initial dial

--- app/src/main/java/skelterjohn/mixedmeter/MainActivity.kt
+++ app/src/main/java/skelterjohn/mixedmeter/MainActivity.kt
@@ -4,13 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import skelterjohn.mixedmeter.ui.theme.MixedMeterTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,11 +25,18 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MixedMeterTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    containerColor = Color.Gray
+                ) { innerPadding ->
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        CircleDisplay()
+                    }
                 }
             }
         }
@@ -31,10 +44,13 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun CircleDisplay() {
+    Box(
+        modifier = Modifier
+            .size(200.dp)
+            .border(width = 2.dp, color = Color.White, shape = CircleShape)
+            .padding(2.dp)
+            .border(width = 8.dp, color = Color.Black, shape = CircleShape)
     )
 }
 
@@ -42,6 +58,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     MixedMeterTheme {
-        Greeting("Android")
+        CircleDisplay()
     }
 }
