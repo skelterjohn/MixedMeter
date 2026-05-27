43fe4c10f43f7e18a5d96c83f5ff01408d972902
John Asmuth <jasmuth@gmail.com>
2026-05-26 21:07:04 -0400

    push version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "skelterjohn.mixedmeter"
         minSdk = 24
         targetSdk = 36
-        versionCode = 5
-        versionName = "1.1.3"
+        versionCode = 6
+        versionName = "1.1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
